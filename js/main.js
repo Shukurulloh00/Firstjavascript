e03c8c299a9b4eba44a75c81505365fad37e3e10
Shukurullo00 <shukurullohkhaydarov@gmail.com>
2022-08-18 10:36:39 +0300

    boshqatdan git push qildm

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ user.practice = prompt ("Maqsadingiz uchun oxirgi qilgan ishingiz");
 
 
 alert(`Assalomu alaykum,
-// Ism :${user.firstname}
-// Familya: ${user.lastname}
-// Maqsadingiz ${user.target}
-// Maqsadingizga qachon yetaman deb o'ylaysiz: ${user.time}
-// Maqsadinggizga erishish uchun nimalar qilyapsiz: ${user.action}
-//Maqsadingiz uchun oxirgi qilgan ishingiz : ${user.practice}
+Ism :${user.firstname}
+Familya: ${user.lastname}
+Maqsadingiz ${user.target}
+ Maqsadingizga qachon yetaman deb o'ylaysiz: ${user.time}
+Maqsadinggizga erishish uchun nimalar qilyapsiz: ${user.action}
+Maqsadingiz uchun oxirgi qilgan ishingiz : ${user.practice}
 `);
 console.log(user)
